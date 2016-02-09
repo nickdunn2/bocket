2038e17bfdfc2336b2cae541cbb8a92818819d78
Nick Dunn <nickdunn2@gmail.com>
2016-02-09 13:48:30 -0600

    Add table migrations

---
+++ database/migrations/2016_02_09_182052_create_bookmark_tag_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBookmarkTagTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookmark_tag', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('tag_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('bookmark_tag');
+    }
+}
